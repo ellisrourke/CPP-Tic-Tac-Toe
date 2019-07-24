c231cc3f2a732ed2c9c43906ed019088b745cc10
ellisrourkePC <ellisrourke@gmail.com>
2019-07-24 17:43:19 +1000

    fgh

--- main.cpp
+++ main.cpp
@@ -5,21 +5,23 @@
 #include <algorithm>
 //new from clion 11:11
 using namespace std;
-class Board{
+class Tictactoe{
 public:
     //constructor method
-    Board(){
+    Tictactoe(){
         srand(time(nullptr));
     }
 
     //data members
     string board[9] = {"1","2","3","4","5","6","7","8","9"};
-    vector<int> filled = {};
-    int totalTurns =0;
-    bool gameOver = false;
+    vector<int> filled = {}; // vector of unavailable board positions
+    int totalTurns =0; // total number of turns taken in the game
+    bool gameOver = false; // current state of the game
 
     //member functions
-   int inArray(vector<int> &arr,int check) {
+
+    //checks if a value is in a vector
+   int inArray(vector<int> arr,int check) {
     if(find(arr.begin(), arr.end(), check) != arr.end()){
           return 1;
         } else {
@@ -27,7 +29,9 @@ public:
         }
     }
 
+    //outputs the winner of the game and sets the game status to over
    void outputWinner(int i){
+
       if(board[i] == "X"){
         cout << "Computer Wins" << endl;
       } else {
@@ -36,45 +40,47 @@ public:
       gameOver = true;
     }
 
+
+    //checks for all possible winning combinations on the Board
+    //calls outputWinner method if a winning combination is found
    int checkWin(){
       for(int i=0;i<9;i+=3){
-        //Check for horizontal wins
+        //Check for horizontal wins//
         if(board[i] == board[i+1] && board[i] == board[i+2]){
           outputWinner(i);
-          gameOver = true;
           return 1;
           }
         }
 
-        //Check for vertical wins
+        //Check for vertical wins//
         for(int j=0;j<3;j++){
-          //Check For horizontal wins
           if(board[j] == board[j+3] && board[j] == board[j+6]){
             outputWinner(j);
-            //gameOver = true;
             return 1;
           }
       }
 
-
-
-        //Check for diagonalWins
+        //Check for diagonalWins//
       if(board[0]==board[4] && board[0]==board[8]){
         outputWinner(0);
+        return 1;
       } else if(board[2]==board[4]&&board[2]==board[6]){
         outputWinner(2);
+        return 1;
       }
+      return 0;
     }
 
+    //takes a board position and a players char indicator
+    //updates the board array with the given move
    void updateBoard(int i,char player){
         board[i] = player;
         filled.push_back(i);
         totalTurns++;
         checkWin();
-        //cout << totalTurns << endl;
-
     }
 
+    //displays the current state of the board with formatting
    void displayBoard(){
         for(int i=1;i<10;i++){
             cout << " | " << board[i-1];
@@ -82,10 +88,12 @@ public:
                 cout << " |" << endl;
             }
         }
-        cout << "-----------------------";
-        cout << endl;
+        cout << "-----------------------" << endl;
     }
 
+    // takes input from the user, checks the input is valid
+    // and the position chosen is unavailable
+    // then updates and displays the board
    void humanTurn(){
       if(gameOver == false){
       int pos=-1;
@@ -99,14 +107,17 @@ public:
         }
       }
 
+      // once the computer has chosen a turn, this method is called
+      // to update the board, display the board and inform
+      // the player of the board postion that was chosen
    void submitComputerTurn(int x){
      updateBoard(x,'X');
      displayBoard();
-     cout << "The computer chose position " << x << endl;
+     cout << "The computer chose position " << x+1 << endl;
    }
 
    int computerTurn(){
-     //check for possible horizontal moves
+     //check for possible horizontal moves//
      for(int i=0;i<9;i+=3){
        if(board[i]==board[i+1] && inArray(filled,i+2)==0){
          submitComputerTurn(i+2);
@@ -119,8 +130,8 @@ public:
          return 1;
        }
      }
-     //////////////////////////////////////////////
-     //check vertical moves
+
+     //check vertical moves//
      for(int j=0;j<3;j++){
        if(board[j]==board[j+3] && inArray(filled,j+6)==0){
          submitComputerTurn(j+6);
@@ -159,6 +170,7 @@ public:
              return 1;
            }
   }
+  // if no moves found, choose random board position
      int num = rand() % 9;
      while(inArray(filled,num)==1){
        num = rand() % 9;
@@ -166,7 +178,7 @@ public:
       submitComputerTurn(num);
 }
 
-   int play(){
+int play(){
         displayBoard();
         while(totalTurns<9){
           if(gameOver==false){
@@ -180,19 +192,13 @@ public:
         }
     }
 
+
 };
 
 
 int main() {
-    Board myBoard;
-    myBoard.play();
-
-
-
-
-
-
-
+    Tictactoe mygame;
+    mygame.play();
 
 
 }
